038380217c8cd7eb0b855ece8422888b9ca61321
lisper <leyapin@gmail.com>
2017-07-27 02:35:17 +0800

    update, clean

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ var nav;
 var scrollTop = 0;
 var title;
 window.onload = function () {
-    //            msg = document.getElementById("msg");
     nav = document.getElementsByClassName("nav-main")[0];
     title = document.getElementById("title");
 }
 document.addEventListener("scroll", function () {
-    //            msg.textContent = ""+document.body.scrollHeight+","
     scrollTop = document.body.scrollTop; // 0~200
     var height = (200-scrollTop); // 200~0
     // nav.style.minHeight = height+"px";
